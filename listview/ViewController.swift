5472566f19002fead9238a58b8dd6bb6dfd1024b
Tigris Solutions India Pvt Ltd <tigris@tigriss-mac-pro.local>
2018-08-21 11:19:28 +0530

    Initial Commit

---
+++ listview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  listview
+//
+//  Created by Tigris Solutions India Pvt Ltd on 21/08/18.
+//  Copyright © 2018 listview. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
